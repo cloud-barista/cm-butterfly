9551e08ed1b6f46493d0aa002a3d963d14ff8f35
hippo-an <dkstpgud@gmail.com>
2024-09-25 14:56:51 +0900

    add create source group with connections api

--- api/actions/source.go
+++ api/actions/source.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"api/handler"
+	"log"
 
 	"github.com/gobuffalo/buffalo"
 )
@@ -11,12 +12,31 @@ func AgentAndConnectionCheck(c buffalo.Context) error {
 	commonRequest := &handler.CommonRequest{}
 	err := c.Bind(commonRequest)
 	if err != nil {
-		commonRes := handler.CommonResponseStatusBadRequest(nil)
+		commonRes := handler.CommonResponseStatusBadRequest(err.Error())
 		return c.Render(commonRes.Status.StatusCode, r.JSON(commonRes))
 	}
 	result, err := handler.AgentAndConnectionCheck(c, commonRequest)
 	if err != nil {
-		commonRes := handler.CommonResponseStatusInternalServerError(nil)
+		commonRes := handler.CommonResponseStatusInternalServerError(err.Error())
+		return c.Render(commonRes.Status.StatusCode, r.JSON(commonRes))
+	}
+
+	commonRes := handler.CommonResponseStatusOK(result)
+	return c.Render(commonRes.Status.StatusCode, r.JSON(commonRes))
+}
+
+func RegisterSourceGroup(c buffalo.Context) error {
+
+	commonRequest := &handler.CommonRequest{}
+	err := c.Bind(commonRequest)
+	if err != nil {
+		commonRes := handler.CommonResponseStatusBadRequest(err.Error())
+		return c.Render(commonRes.Status.StatusCode, r.JSON(commonRes))
+	}
+	result, err := handler.RegisterSourceGroup(c, commonRequest)
+	if err != nil {
+		log.Println(err)
+		commonRes := handler.CommonResponseStatusInternalServerError(err.Error())
 		return c.Render(commonRes.Status.StatusCode, r.JSON(commonRes))
 	}
 
